4d1ac34b859480649333944e307cf0f46907442b
Mykelsown <mykelsown@gmail.com>
2026-01-13 10:49:19 +0100

    updated the stats component UI to display some certain messages when the item is empty, when the item is not fully packed and when the item is fully packed

--- src/App.js
+++ src/App.js
@@ -161,6 +161,26 @@ function Stats({ itemsArr }) {
   let numOfPackedItems = itemsArr.slice().filter((item) => item.packed);
   let numOfUnpackedItems = itemsArr.slice().length - numOfPackedItems.length;
 
+  if (itemsArr.slice().length === 0) {
+    return(
+      <footer className="stats">
+      <em>
+        📌Start adding items to your list 🗳
+      </em>
+    </footer>
+    )
+  }
+
+  if (numOfUnpackedItems === 0 && numOfPackedItems.length > 0) {
+    return(
+      <footer className="stats">
+      <em>
+        Yay, you are all packed and ready for the trip 😎. LFG😋
+      </em>
+    </footer>
+    )
+  }
+
   const getPercentageOfUnpacked = () =>
     (numOfUnpackedItems * 100) / itemsArr.slice().length;
 
@@ -168,7 +188,7 @@ function Stats({ itemsArr }) {
     <footer className="stats">
       <em>
         You have packed {numOfPackedItems.length} items. You have{" "}
-        {itemsArr.slice().length - numOfPackedItems.length} items left({getPercentageOfUnpacked()}%)
+        {itemsArr.slice().length - numOfPackedItems.length} items left({getPercentageOfUnpacked().toFixed(2)}%)
         unpacked
       </em>
     </footer>
